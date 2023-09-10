18aebdb7f94aac245f742387ba3737375ed7832f
mdazzyy <mdazzyy@gmail.com>
2023-09-10 11:41:27 +0530

    type conversion

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,19 @@
+// let score = "33as";
+
+// console.log(typeof score);
+
+// let numberInScore = Number(score);
+// console.log(numberInScore);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "azhar" = true
+
+let digit = 33;
+let stringDigit = String(digit);
+console.log(stringDigit);
+console.log(typeof stringDigit);
