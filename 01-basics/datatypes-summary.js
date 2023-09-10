190ca9a8822cd7a080884f9255a25493a5e9f6b8
mdazzyy <mdazzyy@gmail.com>
2023-09-10 12:30:03 +0530

    datatype summary

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+// Primitive
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const user = "azhar";
+
+const digit = 10;
+const score = 20.8;
+
+const isLoggedIn = true;
+
+let userEmail;
+
+const outsideTemp = null;
+
+const bigInt = 54646586868;
+
+// Non-Primitive/Reference
+// Array, Object, Function
+
+const superHeros = ["iron man", "thor"];
+
+const obj = {
+  name: "azhar",
+  age: 30,
+};
+
+const myFunction = function () {
+  console.log("Hello World");
+};
