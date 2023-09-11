aec370388d34b79419e69003a95899430cd0ef30
mdazzyy <mdazzyy@gmail.com>
2023-09-11 20:07:37 +0530

    numbers and maths

---
+++ 01-basics/06-nums_and_math.js
@@ -0,0 +1,27 @@
+const score = 300;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const decimalNum = 124.8967;
+// console.log(decimalNum.toPrecision(3));
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// -------------------------- Maths ---------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2)); // output is 5
+// console.log(Math.floor(4.8)); // output is 4
+// console.log(Math.min(2, 4, 5, 7));
+// console.log(Math.max(2, 4, 5, 7));
+
+console.log(Math.random()); // value is always 0 to 1
+console.log(Math.floor(Math.random() * 10 + 1));
