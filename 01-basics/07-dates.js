f02906d8b112592f96bd58e8fded5466beb4269b
mdazzyy <mdazzyy@gmail.com>
2023-09-11 21:08:05 +0530

    dates

---
+++ 01-basics/07-dates.js
@@ -0,0 +1,13 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString()); // output: Mon Sep 11 2023 20:23:49 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString()); // output: Mon Sep 11 2023
+// console.log(myDate.toISOString()); // output: 2023-09-11T14:56:13.031Z
+// console.log(myDate.toJSON()); // output: 2023-09-11T14:57:29.321Z
+// console.log(myDate.toLocaleDateString()); // output: 9/11/2023
+
+// let timeStamp = Date.now();
+// console.log(timeStamp);
+
+console.log(myDate.getDate());
