586c242875a93f566d51bcbde4012ac1a267f4e9
mdazzyy <mdazzyy@gmail.com>
2023-09-12 18:54:07 +0530

    rename all files and folders

---
+++ 02_basics/01_array.js
@@ -0,0 +1,25 @@
+// array
+
+const myArr = [1, 3, 5, 7, 9, 3];
+const myHeroes = ["iron man", "thor"];
+
+const myArr2 = new Array(1, 2, 3, 4, 5);
+
+// console.log(myArr[0]);
+
+// Array methods
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+
+// myArr.unshift(10);
+// myArr.shift();
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+const newArr = myArr.join();
+
+console.log(myArr);
+console.log(newArr);
