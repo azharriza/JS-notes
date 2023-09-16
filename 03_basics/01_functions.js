6dc55c6171e031e8c55354d88f80b3a68a27c583
mdazzyy <mdazzyy@gmail.com>
2023-09-16 20:48:11 +0530

    object handler

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,3 +33,25 @@ function loginUserMessage(username) {
 }
 
 // console.log(loginUserMessage(""));
+
+// rest operator ...num1
+// rest operator returns array
+
+function calculateCartPrice(...price) {
+    return price;
+}
+
+// console.log(calculateCartPrice(200, 500, 700, 900));
+
+const user = {
+    username: "azhar",
+    price: 299,
+};
+
+function objectHandler(anyObject) {
+    console.log(
+        `Username is ${anyObject.username} and price is ${anyObject.price}`
+    );
+}
+
+// objectHandler(user);
