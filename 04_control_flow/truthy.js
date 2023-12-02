f8efa785772f451895266af331d091f9095c2df9
Azhar <mdazzyy@gmail.com>
2023-12-02 20:30:09 +0530

    control flow

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,45 @@
+// const userEmail = -1
+
+// if (userEmail){
+//     console.log("Got user Email");
+// }else{
+//     console.log("Dont't have user Email");
+// }
+
+// Falsy values
+// false, 0, -0, BigInt 0N, "", null, undefined, NaN
+
+// Truthu values
+// "0", 'false', " ", [], {}, function(){}
+
+// check array is empty or not
+
+// const userName = []
+// if (userName.length === 0){
+//     console.log("Array is empty");
+// }
+
+// check object is empty or not
+
+// const emptyObj = {}
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty");
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+
+// console.log(val1);
+
+// --------------------------------------------------
+
+// Terniary Operator
+// condition ? true : false
+
+const teaPrice = 50
+teaPrice <= 30 ? console.log("less than 30"): console.log("more tha 30");
+
